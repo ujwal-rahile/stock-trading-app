5a67c32ece796e99f5a5fbfaf210b2ff91432233
Ujwal Rahile <ujwalrahile02@gmail.com>
2025-10-02 09:38:56 +0530

    Initial Setup

---
+++ config-repo/src/test/java/com/stockapp/configrepo/ConfigRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockapp.configrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
