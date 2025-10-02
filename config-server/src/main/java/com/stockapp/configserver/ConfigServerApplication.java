a27e61fa897290aef7f528fcede623fd8d882c2c
Ujwal Rahile <ujwalrahile02@gmail.com>
2025-10-02 10:13:31 +0530

    added config for ms

--- config-server/src/main/java/com/stockapp/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/stockapp/configserver/ConfigServerApplication.java
@@ -1,9 +1,11 @@
-package com.stockapp.config_server;
+package com.stockapp.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
